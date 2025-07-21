25de6f098695b290119559ce6543b187b09fe5b7
Dipesh Malviya <malviyadipesh71@gmail.com>
2025-07-21 14:34:35 +0530

    Add solutions for Assignment-5 questions 29 and 30
    Added q29.java and q30.java implementing series generation based on user input. Both files prompt for the number of terms and print a sequence of numbers formed by repeated '1's. q30.java includes both commented and active code for different output formats.

---
+++ Java/Assignment-5/q30.java
@@ -0,0 +1,62 @@
+/*
+import java.util.Scanner;
+class Series{
+public static void main(String []args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number how many terms you want (1 to 19): ");
+int n=sc.nextInt();
+int i=1;
+long count=1l;
+
+while(i<=n){
+if(i==1)
+System.out.print(count);
+else
+System.out.print("+"+count);
+
+
+count=count*10+1;
+i++;
+
+
+
+}
+
+}
+}
+
+*/
+
+
+
+import java.util.Scanner;
+class Series{
+public static void main(String []args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number how many terms you want (1 to 19): ");
+int n=sc.nextInt();
+int i=1;
+long count=1l;
+
+while(i<=n){
+
+System.out.print(count "+");
+count=count*10+1;
+i++;
+
+
+
+}
+
+}
+}
+
+
+
+
+
+
+
+
+
+
